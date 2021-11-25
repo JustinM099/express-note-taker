0e94a69ff1979477c4ba80d3aec281abd8c1c9f1
JustinM099 <justinm099@mac.com>
2021-11-25 13:14:36 -0800

    updating routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,10 @@
+const express = require('express')
+const path = require('path')
+const routes = require('express').Router()
 
 
-app.get('/notes', (req, res) =>
+routes.get('/notes', (req, res) =>
 res.sendFile(path.join(__dirname, '../public/index.html'))
 );
+
+module.exports = htmlRoutes
